64311cf845888cbf7db79a4ec43423fde3c3bcc7
bsfranca2 <brunofranca552@hotmail.com>
2020-10-16 17:32:26 -0300

    remove emoji mart

--- src/components/bottom-bar/chat-emoji.component.js
+++ src/components/bottom-bar/chat-emoji.component.js
@@ -1,12 +1,10 @@
 import { Component } from 'preact';
-import { Picker } from 'emoji-mart';
-import 'emoji-mart/css/emoji-mart.css';
 import ChatIcon from '../chat-icon.component';
 import ChatIconButton from '../chat-icon-button.component';
 
 function ChatEmojiContainer({ show }) {
 	if (!show) return null;
-	return <div class={'ab'}><Picker set={'apple'} /></div>;
+	return <></>;
 }
 
 export default class ChatEmoji extends Component {
