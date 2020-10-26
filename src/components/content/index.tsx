cfe9dfa84f05a245e8b873107e3429548c35136f
Bruno França <bsfranca2@gmail.com>
2020-10-26 06:59:55 -0300

    Scrollar para baixo automaticamente em nova msg

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,12 +1,34 @@
 import { FunctionalComponent, h } from 'preact';
+import { useState } from 'preact/hooks';
 import { useChat } from '../../contexts/chat';
 import { useMessages } from '../../contexts/messages';
+import { Message } from '../../models/message';
 import MessageList from '../message-list';
 import style from './style.css';
 
 const Content: FunctionalComponent = () => {
+	const [lastMessages, setLastMessages] = useState<ReadonlyArray<Message>>([]);
+
 	const { me } = useChat();
 	const { messages } = useMessages();
+
+	if (lastMessages.length === 0) {
+		setLastMessages(messages);
+	} else if (lastMessages.length !== messages.length) {
+		const main = document.querySelector('main');
+		if (main) {
+			setTimeout(() => {
+				const tolerance = 150;
+				const contentInView = main.scrollHeight - main.clientHeight;
+				const scrollWithTolerance = main.scrollTop + tolerance;
+				if (scrollWithTolerance >= contentInView) {
+					main.scrollTop = main.scrollHeight - main.clientHeight;
+				}
+			}, 0);
+		}
+		setLastMessages(messages);
+	}
+
 	return (
 		<main class={style.content}>
 			<MessageList username={me.name} messages={messages} />
