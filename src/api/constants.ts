5bcff64428ff63d22be94994ac4215454eccbe1b
Bruno França <39228171+bsfranca2@users.noreply.github.com>
2020-10-30 21:13:59 -0300

    backend dev api

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://localhost:9292';
+export const baseUrl = 'http://23.111.140.206:30660/';
 
 export const API_CHAT = `${baseUrl}/chat`;
 export const API_MESSAGES = `${baseUrl}/messages`;
